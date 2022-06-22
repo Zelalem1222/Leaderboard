46e4fd2c4a772f89249cfe268d426edce95e383e
Zelalem1222 <zelalemfissha394@gmail.com>
2022-06-22 03:37:29 +0300

    Add list of score

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 import './style.css';
+import scoreList from './scoreList';
 
-alert('hello world')
+
+function display(name , score){
+    document.querySelector('.list').innerHTML += `<ul >        
+    <li>${name}:${score}</li></ul>`
+  }
+scoreList.forEach(score => {
+    console.log(score.name + ':' + score.score)
+    display(score.name, score.score)
+})
+
+
---
+++ src/scoreList.js
@@ -0,0 +1,29 @@
+
+const scoreList = [
+    {
+        name: 'zelalem',
+        score: 100
+    },
+    {
+        name: 'Abel',
+        score: 100
+    },
+    {
+        name: 'Dozie',
+        score: 100
+    },
+    {
+        name: 'Angela',
+        score: 100
+    },
+    {
+        name: 'Bekele',
+        score: 100
+    },
+    {
+        name: 'Jhon',
+        score: 100
+    },
+]
+
+export default scoreList;
