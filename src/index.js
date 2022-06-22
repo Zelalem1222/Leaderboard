2c8518b731f7cd211604308c0217198f5a96fc61
Zelalem1222 <zelalemfissha394@gmail.com>
2022-06-22 04:41:13 +0300

    Install and fix linters

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import './style.css';
 import scoreList from './scoreList';
 
+const display = (name, score) => {
+  document.querySelector('.list').innerHTML += `<ul >        
+    <li>${name}:${score}</li></ul>`;
+};
 
-function display(name , score){
-    document.querySelector('.list').innerHTML += `<ul >        
-    <li>${name}:${score}</li></ul>`
-  }
-scoreList.forEach(score => {
-    console.log(score.name + ':' + score.score)
-    display(score.name, score.score)
-})
-
-
+scoreList.forEach((score) => {
+  display(score.name, score.score);
+});
--- webpack.config.js
+++ webpack.config.js
@@ -2,21 +2,21 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
+  mode: 'development',
   entry: './src/index.js',
   devServer: {
     static: './dist',
   },
   plugins: [
     new HtmlWebpackPlugin({
-     template: './src/index.html'
+      template: './src/index.html',
     }),
   ],
   output: {
     filename: '[name].js',
     path: path.resolve(__dirname, 'dist'),
   },
-    module: {
+  module: {
     rules: [
       {
         test: /\.css$/i,
