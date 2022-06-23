2b51b0addd6e6939eb15cbcde29b2b2758da7656
Zelalem1222 <zelalemfissha394@gmail.com>
2022-06-23 08:28:19 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,38 +1,36 @@
 import './style.css';
 import { display } from './modules/display';
 
-
-
 const submit = document.getElementById('submit');
 const refresh = document.getElementById('refresh');
 const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/a1jL7pBQ8HR9eL9EL2zG/scores/'
 
-submit.addEventListener('click' , (e) => {
-  e.preventDefault;
-  const user = document.getElementById('user').value.trim();
-  const score = document.getElementById('score').value.trim();
-  fetch(url ,{
-    method : 'POST',
-    body : JSON.stringify({
-      user: user,
-      score:score,
-    }),
-    headers : {
-      "Content-Type": "application/json; charset=UTF-8"
-    }
-  })
-  .then((response) => {
-    return response.json();
-  })
-  .then((data) => {
-    console.log(data);
+
+
+submit.addEventListener('click' , () => {
+    const user = document.getElementById('user')
+    const score = document.getElementById('score')
+    fetch(url ,{
+      method : 'POST',
+      body : JSON.stringify({
+        user: user.value.trim(),
+        score:score.value.trim(),
+      }),
+      headers : {
+        "Content-Type": "application/json; charset=UTF-8"
+      }
+    })
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+    })
+    user.value = '';
+    score.value = '';
   })
 
-  user.value = '';
-  score.value= '';
-})
-
-async function populate() {
+async function getScore() {
   const requestURL = url;
   const request = new Request(requestURL);
 
@@ -46,6 +44,6 @@ async function populate() {
 }
 
 
+refresh.addEventListener('click' , getScore)
 
-refresh.addEventListener('click' , populate)
 
