9cd702628e12bb60e801901ab4ef35aeb44011ef
Zelalem1222 <zelalemfissha394@gmail.com>
2022-06-23 08:35:21 +0300

    Fix linter errors

--- src/index.js
+++ src/index.js
@@ -1,49 +1,42 @@
 import './style.css';
-import { display } from './modules/display';
+import display from './modules/display';
 
 const submit = document.getElementById('submit');
 const refresh = document.getElementById('refresh');
-const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/lGcv4uHBWKY5C0XB2d3f/scores'
+const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/lGcv4uHBWKY5C0XB2d3f/scores';
 
-
-
-submit.addEventListener('click' , () => {
-    const user = document.getElementById('user')
-    const score = document.getElementById('score')
-    fetch(url ,{
-      method : 'POST',
-      body : JSON.stringify({
-        user:user.value.trim(),
-        score:score.value.trim()
-      }),
-      headers : {
-        "Content-Type": "application/json; charset=UTF-8"
-      }
-    })
-    .then((response) => {
-      return response.json();
-    })
+submit.addEventListener('click', () => {
+  const user = document.getElementById('user');
+  const score = document.getElementById('score');
+  fetch(url, {
+    method: 'POST',
+    body: JSON.stringify({
+      user: user.value.trim(),
+      score: score.value.trim(),
+    }),
+    headers: {
+      'Content-Type': 'application/json; charset=UTF-8',
+    },
+  })
+    .then((response) => response.json())
     .then((data) => {
       console.log(data);
-    })
-    user.value = '';
-    score.value = '';
-  })
+    });
+  user.value = '';
+  score.value = '';
+});
 
 async function getScore() {
   const requestURL = url;
   const request = new Request(requestURL);
 
   const response = await fetch(request);
-  const scores =  await response.json();
-  const result = scores.result
+  const scores = await response.json();
+  const { result } = scores;
 
-  result.forEach(score => {
-    display(score.user , score.score);
-  }) 
+  result.forEach((score) => {
+    display(score.user, score.score);
+  });
 }
 
-
-refresh.addEventListener('click' , getScore)
-
-
+refresh.addEventListener('click', getScore);
--- src/modules/display.js
+++ src/modules/display.js
@@ -1,6 +1,6 @@
-export const display = (name, score) => {
-    document.querySelector('.list').innerHTML += `<ul >        
+const display = (name, score) => {
+  document.querySelector('.list').innerHTML += `<ul >        
     <li>${name} : ${score}</li></ul>`;
-  };
-  
- 
+};
+
+export default display;
